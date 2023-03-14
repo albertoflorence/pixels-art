f6334ee8b5a2eb2854f40a5d64e7d64c2b0ab9bd
albertoflorence <alberto.sabatier@gmail.com>
2023-03-14 16:41:07 -0300

    requisito 6

--- script.js
+++ script.js
@@ -58,4 +58,31 @@ const PaletteColor = () => {
   return element;
 };
 
+const Cell = (index) => {
+  const element = document.createElement('div');
+  element.classList.add('pixel');
+  element.style.backgroundColor = 'white';
+  element.id = `cell-${index}`;
+  return element;
+};
+
+const Board = () => {
+  const element = document.createElement('div');
+  element.id = 'pixel-board';
+  const rows = Array.from({ length: 5 });
+  const lines = Array.from({ length: 5 });
+
+  rows.forEach((_, rowIndex) => {
+    const row = document.createElement('div');
+    row.classList.add('row');
+    element.appendChild(row);
+    lines.forEach((__, lineIndex) => {
+      row.appendChild(Cell(rowIndex * rows.length + lineIndex));
+    });
+  });
+
+  return element;
+};
+
 document.body.appendChild(PaletteColor());
+document.body.appendChild(Board());
