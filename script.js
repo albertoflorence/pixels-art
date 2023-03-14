f527117eff4dca037cd4d6dd68457ee4de3d2471
albertoflorence <alberto.sabatier@gmail.com>
2023-03-14 15:32:42 -0300

    requisito 4

--- script.js
+++ script.js
@@ -1,15 +1,41 @@
+const random = (min, max) => Math.random() * (max - min) + min;
+
+const ButtonRandomColor = () => {
+  const element = document.createElement('button');
+  element.id = 'button-random-color';
+  element.innerText = 'Cores aleatórias';
+
+  element.addEventListener('click', () => {
+    const getRandomColor = () =>
+      `rgb(${random(0, 255)},${random(0, 255)},${random(0, 255)})`;
+    document.querySelectorAll('.color').forEach((item, index) => {
+      const eslint = item;
+      eslint.style.backgroundColor = index === 0 ? 'black' : getRandomColor();
+    });
+  });
+
+  return element;
+};
+
+const Color = (color) => {
+  const element = document.createElement('div');
+  element.classList.add('color');
+  element.style.backgroundColor = color;
+
+  return element;
+};
+
 const PaletteColor = () => {
-  const palette = document.createElement('div');
-  palette.id = 'color-palette';
+  const element = document.createElement('div');
+  element.id = 'color-palette';
   const colors = ['black', 'purple', 'orange', 'green'];
 
   colors.forEach((color) => {
-    const div = document.createElement('div');
-    div.classList.add('color');
-    div.style.backgroundColor = color;
-    palette.appendChild(div);
+    element.appendChild(Color(color));
   });
-  return palette;
+
+  element.appendChild(ButtonRandomColor());
+  return element;
 };
 
 document.body.appendChild(PaletteColor());
