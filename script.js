7c8bccf26e1826b66165c54a9aa0366d94ce66c7
albertoflorence <alberto.sabatier@gmail.com>
2023-03-14 16:07:18 -0300

    requisito 5

--- script.js
+++ script.js
@@ -1,16 +1,28 @@
-const random = (min, max) => Math.random() * (max - min) + min;
+const random = (min, max) => Math.round(Math.random() * (max - min) + min);
 
-const ButtonRandomColor = () => {
+const getRandomColor = () =>
+  `rgb(${random(0, 255)},${random(0, 255)},${random(0, 255)})`;
+
+const getStorageItem = (key) => {
+  const value = localStorage.getItem(key);
+  return value && JSON.parse(value);
+};
+
+const setStorageItem = (key, value) => {
+  localStorage.setItem(key, JSON.stringify(value));
+};
+
+const ButtonRandomColor = (saveColors) => {
   const element = document.createElement('button');
   element.id = 'button-random-color';
   element.innerText = 'Cores aleatórias';
 
   element.addEventListener('click', () => {
-    const getRandomColor = () =>
-      `rgb(${random(0, 255)},${random(0, 255)},${random(0, 255)})`;
     document.querySelectorAll('.color').forEach((item, index) => {
+      const color = index === 0 ? 'black' : getRandomColor();
       const eslint = item;
-      eslint.style.backgroundColor = index === 0 ? 'black' : getRandomColor();
+      eslint.style.backgroundColor = color;
+      saveColors(index, color);
     });
   });
 
@@ -28,13 +40,21 @@ const Color = (color) => {
 const PaletteColor = () => {
   const element = document.createElement('div');
   element.id = 'color-palette';
-  const colors = ['black', 'purple', 'orange', 'green'];
+  const colors = getStorageItem('colorPalette') || [
+    'black',
+    'purple',
+    'orange',
+    'green',
+  ];
 
-  colors.forEach((color) => {
-    element.appendChild(Color(color));
-  });
+  colors.forEach((color) => element.appendChild(Color(color)));
 
-  element.appendChild(ButtonRandomColor());
+  const handleChangeColor = (index, color) => {
+    colors[index] = color;
+    setStorageItem('colorPalette', colors);
+  };
+
+  element.appendChild(ButtonRandomColor(handleChangeColor));
   return element;
 };
 
